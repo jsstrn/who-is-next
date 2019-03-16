a88c40b8676855a2099c4c92e39beb76fa29c009
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-03-16 11:52:20 +0800

    Refactor - create separate modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import names from "./names.js";
-import rng from "./randomizer.js";
+import names from "../lib/names.js";
+import rng from "../lib/randomizer.js";
+import isWanted from "../lib/isWanted.js";
+import {
+  createNameElement,
+  getCurrentNameElement,
+  removeNameElement,
+  addNameElement
+} from "../lib/element.js";
 
 const getName = names => {
   const max = names.length - 1;
@@ -17,51 +24,18 @@ const getNonRecurringName = currentName => {
   return nextName;
 };
 
-const getCurrentNameElement = () => {
-  return document.querySelector(".animated").textContent;
-};
-
-const removeNameElement = () => {
-  const div = document.querySelector(".animated");
-  div.remove();
-};
-
-const createNameElement = name => {
-  const div = document.createElement("div");
-  div.classList.add("animated", "bounceIn");
-  div.textContent = name;
-  return div;
-};
-
-const addNameElement = div => {
-  const main = document.querySelector("main");
-  const footer = document.querySelector("footer");
-  main.insertBefore(div, footer);
-};
-
-const handleEvent = event => {
-  const keyboard = {
-    enter: 13,
-    space: 32
-  };
-
-  const isClickEvent = event.type === "click";
-  const isTouchEvent = event.type === "touchstart";
-  const isSpaceKey = event.type === "keypress" && event.which == keyboard.space;
-  const isEnterKey =
-    event.type === "keypress" && event.which === keyboard.enter;
-
-  const isWantedEvent =
-    isClickEvent || isEnterKey || isSpaceKey || isTouchEvent;
-
-  if (!isWantedEvent) return;
-
+const selectWhoIsNext = () => {
   const currentName = getCurrentNameElement();
   const name = getNonRecurringName(currentName);
   removeNameElement();
 
   const nameElement = createNameElement(name);
   addNameElement(nameElement);
+};
+
+const handleEvent = event => {
+  if (!isWanted(event)) return;
+  selectWhoIsNext();
 };
 
 document.ontouchstart = e => e.preventDefault();
